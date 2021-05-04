39afbc3fa86b1ac23d16ff586fa6545f948835d3
Lean <leandrohornos@gmail.com>
2021-05-04 19:24:39 -0300

    estructura semantica e informacion

--- src/Components/Query.jsx
+++ src/Components/Query.jsx
@@ -104,101 +104,190 @@ const Query = (props) => {
     <React.Fragment>
       <NavigationBar />
       <div className="row">
-        <div className="col-md-3"></div>
-        <div className="col-md-6">
+        <div className="col-md-1"></div>
+        <div className="col-md-7">
           <div className="cont">
-            <h1 className="txt-color-1 text-center">
-              ¿ Cómo varió en dólares ?
-            </h1>
-            <p>
-              Compará como varió a lo largo del tiempo tu salario, tu alquiler,
-              la cuota del préstamo o cualquier otro monto en pesos argentinos
-              en su equivalente en dólares utilizando esta aplicación. Completá
-              los montos en pesos correspondientes a dos fechas distintas y dale
-              al botón "calcular" para ver los resultados.
-            </p>
-            <form>
-              <div className="mb-3">
-                <label htmlFor="old-date" className="form-label">
-                  Fecha inicial:
-                </label>
-                <p className="form-text">
-                  La fecha inicial debe ser posterior a junio de 2012
-                </p>
-                <input
-                  type="date"
-                  className="form-control"
-                  aria-describedby="dateHelp"
-                  id="old-date"
-                  onChange={(e) => {
-                    setOldDate(e.target.value);
-                  }}
-                />
-              </div>
+            <header>
+              <h1 className="txt-color-1 text-center">
+                ¿ Cómo varió en dólares ?
+              </h1>
+              <p>
+                El objetivo de esta página es ayudar a aquellas personas que
+                residen en la República Argentina a comprender cómo varía su
+                poder adquisitivo a lo largo del tiempo, al comparar cifras en
+                pesos convirtiéndolas a otros valores de referencia a la fecha
+                indicada, como ser el dolar o la Unidad de Valor Adquisitivo
+                UVA.
+              </p>
+              <p>
+                ¿Hoy ganás muchos más pesos que hace unos años, pero sentís que
+                comprás mucho menos?{" "}
+                <strong>
+                  {" "}
+                  Compará como variaron entre dos fechas tu salario, tu alquiler,
+                  la cuota del préstamo o cualquier otro monto en pesos
+                  argentinos convirtiéndolos en su equivalente en dólares
+                  utilizando esta aplicación.
+                </strong>{" "}
+                Completá los montos en pesos correspondientes a dos fechas
+                distintas y dale al botón "calcular" para ver los resultados.
+              </p>
+            </header>
+            <section id="seccion-formulario-busqueda">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="old-date" className="form-label">
+                    Fecha inicial:
+                  </label>
+                  <p className="form-text">
+                    La fecha inicial debe ser posterior a junio de 2012
+                  </p>
+                  <input
+                    type="date"
+                    className="form-control"
+                    aria-describedby="dateHelp"
+                    id="old-date"
+                    onChange={(e) => {
+                      setOldDate(e.target.value);
+                    }}
+                  />
+                </div>
 
-              <div className="mb-3">
-                <label htmlFor="old-ammount" className="form-label">
-                  Monto anterior en pesos argentinos:
-                </label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="old-ammount"
-                  value={oldAmmount}
-                  onChange={(e) => {
-                    setOldAmmount(e.target.value);
-                  }}
-                />
-              </div>
+                <div className="mb-3">
+                  <label htmlFor="old-ammount" className="form-label">
+                    Monto anterior en pesos argentinos:
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="old-ammount"
+                    value={oldAmmount}
+                    onChange={(e) => {
+                      setOldAmmount(e.target.value);
+                    }}
+                  />
+                </div>
 
-              <div className="mb-3">
-                <label htmlFor="new-date" className="form-label">
-                  Fecha final:
-                </label>
-                <p className="form-text">
-                  La fecha final debe ser posterior a la inicial y anterior o
-                  igual a la fecha de hoy.
-                </p>
-                <input
-                  type="date"
-                  className="form-control"
-                  aria-describedby="dateHelp"
-                  id="new-date"
-                  onChange={(e) => {
-                    setNewDate(e.target.value);
-                  }}
-                />
-              </div>
+                <div className="mb-3">
+                  <label htmlFor="new-date" className="form-label">
+                    Fecha final:
+                  </label>
+                  <p className="form-text">
+                    La fecha final debe ser posterior a la inicial y anterior o
+                    igual a la fecha de hoy.
+                  </p>
+                  <input
+                    type="date"
+                    className="form-control"
+                    aria-describedby="dateHelp"
+                    id="new-date"
+                    onChange={(e) => {
+                      setNewDate(e.target.value);
+                    }}
+                  />
+                </div>
 
-              <div className="mb-3">
-                <label htmlFor="new-ammount" className="form-label">
-                  Nuevo monto en pesos argentinos:
-                </label>
-                <input
-                  type="number"
-                  className="form-control"
-                  id="new-ammount"
-                  value={newAmmount}
-                  onChange={(e) => {
-                    setNewAmmount(e.target.value);
+                <div className="mb-3">
+                  <label htmlFor="new-ammount" className="form-label">
+                    Nuevo monto en pesos argentinos:
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="new-ammount"
+                    value={newAmmount}
+                    onChange={(e) => {
+                      setNewAmmount(e.target.value);
+                    }}
+                  />
+                </div>
+                <AlertMessages messages={msgs} />
+                <button
+                  type="submit"
+                  className="btn btn-success btn-block"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
                   }}
-                />
-              </div>
-              <AlertMessages messages={msgs} />
-              <button
-                type="submit"
-                className="btn btn-success btn-block"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleSubmit();
-                }}
-              >
-                Calcular
-              </button>
-            </form>
+                >
+                  Calcular
+                </button>
+              </form>
+            </section>
           </div>
         </div>
-        <div className="col-md-3"></div>
+        <div className="col-md-4">
+          <aside></aside>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-7">
+          <main>
+            <section id="informacion-general">
+              <article id="que-es-el-dolar-blue">
+                <h2 className="txt-color-1 text-center">
+                  ¿Qué es el dolar blue?
+                </h2>
+                <p>
+                  El dólar blue, dólar paralelo o dólar negro, son eufemismos
+                  que se utilizan en Argentina para referirse al dólar
+                  estadounidense comprado ilegalmente en el mercado negro. Suele
+                  tener un tipo de cambio que se encuentra muy por encima del
+                  dólar oficial.
+                </p>
+                <p>
+                  Este concepto se empezó a utilizar en el país sudamericano en
+                  2011, como consecuencia de las restricciones, bajo el gobierno
+                  de Cristina Fernández de Kirchner, para la adquisición de
+                  moneda extranjera impuestas por la Administración Federal de
+                  Ingresos Públicos y el Banco Central de la República
+                  Argentina.
+                </p>
+              </article>
+              <article id="que-es-el-valor-uva">
+                <h2 className="txt-color-1 text-center">¿Qué es UVA?</h2>
+                <p>
+                  <strong>
+                    La Unidad de Valor Adquisitivo UVA equivale a la milésima
+                    parte del costo promedio de construcción de un metro
+                    cuadrado de vivienda.
+                  </strong>{" "}
+                  El valor se actualiza diariamente en función a la variación
+                  del CER (Coeficiente de Estabilización de Referencia), basado
+                  en el índice de precios al consumidor. El valor de la UVA en
+                  pesos se publica en el Banco Central de la República
+                  Argentina.
+                </p>
+                <p>
+                  De esta forma, la estrategia detrás de las UVA consiste en que
+                  la misma se ajuste a la evolución del costo de construcción.
+                  Es decir, la UVA se indexa a alguna variable que refleje la
+                  evolución de los precios del sector que genera la necesidad de
+                  tomar créditos hipotecarios. La finalidad que se persigue es
+                  la de expresar los precios de transacciones de largo plazo en
+                  moneda local (el peso), para desacoplarlas de las variaciones
+                  de una extranjera (el dólar). La dificultad es que el flujo de
+                  ingresos de los potenciales tomadores de crédito, se
+                  encuentran expresados en pesos y, por tanto, no necesariamente
+                  ajustan a la misma velocidad o nivel que la UVA.
+                </p>
+                <p>
+                  Asimismo, dado que el CER se calcula de forma diaria y el IPC
+                  (Índice de Precios al Consumidor) de forma mensual (y
+                  desfasada); el primero es un tipo de proyección geométrica del
+                  IPC que calcula –nuevamente- el BCRA. Finalmente, el IPC se
+                  encuentra en función de la variación de precios de una canasta
+                  de bienes y servicios representativa de la población, estimada
+                  por el INDEC.
+                </p>
+              </article>
+            </section>
+          </main>
+        </div>
+        <div className="col-md-4">
+          <aside></aside>
+        </div>
       </div>
     </React.Fragment>
   );
